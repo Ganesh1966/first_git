28d22bd8e5c21ca7c88a203951ab77562916e720
Venkat <ganeshethiraj185@gmail.com>
2021-09-18 12:38:21 +0530

    recipe

---
+++ receipe/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return `
+        <h3><a href="home.html">Home</a></h3>
+
+        <div id="options">
+            <h3><a href="dayreceipe.html">Recipe of the day</a></h3>
+        </div>
+    `
+}
+
+export default navbar
